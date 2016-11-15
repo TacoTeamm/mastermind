f47b9277532f28e0aa55f5e94be251d163e79f68
Petiee <friborg_2800@hotmail.com>
2016-11-15 15:11:35 +0100

    Temp make

--- makeCode.fsx
+++ makeCode.fsx
@@ -46,12 +46,10 @@ let makeCode player =
                 let rnd = System.Random()
                 let mutable ix = 5
                 for i=1 to 4 do
-                    let nmbr = rnd.Next(0, ix)
-                    printfn "%A" codeColorList
+                    nmbr <- rnd.Next(0, ix)
                     makeList <- codeColorList.[nmbr] :: makeList
                     codeColorList <- listRemove nmbr codeColorList
-                    if nmbr >= (codeColorList.Length - 1) then
-                       ix <- ix - 1
+                    ix <- ix - 1
   (*| _ -> failwith "Invalid player"*)
 printfn "Who wants to make the codecombination?\nHuman | Computer"
 let (makePlayer : player) = stringToPlayer (Console.ReadLine())
@@ -75,15 +73,48 @@ let guessCode player =
                 let mutable nmbr = 0
                 let rnd = System.Random()
                 let mutable ix = 5
+                guessList <- []
                 for i=1 to 4 do
-                    let nmbr = rnd.Next(0, ix)
-                    printfn "%A" codeColorList2
+                    nmbr <- rnd.Next(0, ix)
                     guessList <- codeColorList2.[nmbr] :: guessList
                     codeColorList2 <- listRemove nmbr codeColorList2
-                    if nmbr >= (codeColorList2.Length - 1) then
-                       ix <- ix - 1
+                    ix <- ix - 1
   (*| _ -> failwith "Invalid player"*)
 printfn "Who wants to guess the codecombination?\nHuman | Computer"
 let (guessPlayer : player) = stringToPlayer (Console.ReadLine())
 guessCode guessPlayer
 printfn "%A" guessList
+
+let mutable (codeAnswer : answer) = (0, 0)
+let mutable (board1 : board) = []
+let validateCode (tryCode : code) (trueCode : code) =
+  let mutable (hvid: int) = 0
+  for i = 0 to (tryCode.Length - 1) do
+    if List.contains tryCode.[i] trueCode then
+       hvid <- hvid + 1
+  printfn "%A" hvid
+  let mutable (sort: int) = 0
+  if hvid > 0 then
+     for i = 0 to (tryCode.Length - 1) do
+        if tryCode.[i] = trueCode.[i] then
+           hvid <- hvid - 1
+           sort <- sort + 1
+  printfn "Hvid : %A \nSort : %A" hvid sort
+  printfn "%A" guessList
+  codeAnswer <- (hvid, sort)
+  if makeList = guessList then
+     Console.WriteLine "Congratz you won! You guessed the code"
+
+validateCode makeList guessList
+printfn "%A" codeAnswer
+board1 <- ((guessList, codeAnswer) :: board1)
+
+Console.WriteLine "The board so far"
+for i = 0 to 10 do
+  if board1.Length < 10 then
+    Console.WriteLine "Sorry you lost! You didn't guess the code"
+  else guessCode guessPlayer
+       validateCode makeList guessList
+       board1 <- ((guessList, codeAnswer) :: board1)
+       for i = (board1.Length) downto 0 do
+          printfn "%A\n" board1.[i]
